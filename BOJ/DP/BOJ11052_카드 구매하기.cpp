fe1423d45e4000c58accb7c1b6395c15354295d5
cbj2741@gmail.com <cbj2741@naver.com>
2022-06-22 23:39:41 +0900

    Create BOJ11052_카드 구매하기.cpp

---
+++ BOJ/DP/BOJ11052_카드 구매하기.cpp
@@ -0,0 +1,21 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+int dp[1001] = { 0 };
+int p[1001];
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) cin >> p[i];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] = max(dp[i], (dp[i - j] + p[j]));
+        }
+    }
+    cout << dp[n] << "\n";
+    return 0;
+}
