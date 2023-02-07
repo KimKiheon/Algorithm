eb22f702b066daa8311efe940b2fe2bee53a470d
KimKiheon <cbj2741@naver.com>
2023-02-07 10:55:48 +0900

    [Silver I] Title: 하노이 탑, Time: 580 ms, Memory: 73048 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class Main {
 	static int N;
-
+	static StringBuilder sb=new StringBuilder();
 	static void go(int n, int start, int by, int to) {
 		if (n == 1) {
-			System.out.println(start + " " + to);
+			sb.append(start+" "+to+"\n");
 			return;
 		}
 		go(n - 1, start, to, by);
-		System.out.println(start + " " + to);
+		sb.append(start+" "+to+"\n");
 		go(n - 1, by, start, to);
 	}
 
@@ -22,8 +22,9 @@ public class Main {
 		N = scanner.nextInt();
 		BigInteger bigInteger = BigInteger.ONE;
 		System.out.println(bigInteger.shiftLeft(N).subtract(BigInteger.ONE).toString());
-		if (N <= 20)go(N, 1, 2, 3);
-
+		if (N <= 20) {
+			go(N, 1, 2, 3);
+			System.out.println(sb.toString());
+		}
 	}
-
 }
