fd688c4ca4fea43a315ca95655a5bd0c5c89cbec
KimKiheon <cbj2741@naver.com>
2023-02-09 09:04:45 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1000005];
+int main() {
+	int n;
+	cin >> n; 
+	dp[1] = 0; 
+	dp[2] = 1;
+	dp[3] = 1;
+
+	for (int i = 4; i < n + 1; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
+		if (i % 2 == 0)dp[i] = min(dp[i / 2] + 1, dp[i]);
+	}
+	cout << dp[n] << '\n';
+
+}
