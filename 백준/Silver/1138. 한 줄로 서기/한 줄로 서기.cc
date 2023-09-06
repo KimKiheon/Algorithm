d20276c27069ebd0e261f29a2fa0aee54995cac5
KimKiheon <cbj2741@naver.com>
2023-09-06 22:05:00 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int N;
+    cin >> N;
+    vector<int> v(N), answer(N);
+    for(int i = 0; i < N; i++)
+        cin >> v[i];
+    
+    for(int i = 0; i < N; i++) {
+        int j = 0;
+        while(v[i] != 0) { 
+            if(answer[j] == 0) {
+                v[i]--;
+            }
+            j++;
+        }
+        while(answer[j] != 0) j++;
+        answer[j] = i + 1;
+    }
+    
+    for(int i = 0; i < N; i++)
+        cout << answer[i] << ' ';
+ 
+    return 0;
+}
