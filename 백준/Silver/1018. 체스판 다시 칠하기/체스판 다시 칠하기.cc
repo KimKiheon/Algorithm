d49d3ed70b9a7f49f53ec53667595b91a2711bc2
KimKiheon <cbj2741@naver.com>
2023-09-07 21:39:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+using namespace std;
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+string board[50];
+int WB_cnt(int x, int y){
+    int cnt = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (board[x + i][y + j] != WB[i][j])cnt++;
+        }
+    }
+    return cnt;
+}
+int BW_cnt(int x, int y){
+    int cnt = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (board[x + i][y + j] != BW[i][j])cnt++;
+        };
+    }
+    return cnt;
+}
+int main() {
+    int size[2];
+    int cnt;
+    int ans = 1e9;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> board[i];
+    for (int i = 0; i + 8 <= n; i++)
+    {
+        for (int j = 0; j + 8 <= m; j++)
+        {
+            int tmp;
+            tmp = min(WB_cnt(i, j), BW_cnt(i, j));
+            ans = min(tmp, ans);
+        }
+    }
+    cout << ans;
+    return 0;
+}
