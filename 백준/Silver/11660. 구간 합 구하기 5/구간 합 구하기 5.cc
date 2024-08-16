97b5aded08c3ab19e87eafa0aeb6d61d3cba01e3
KimKiheon <cbj2741@naver.com>
2024-08-17 02:37:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,31 +1,28 @@
 #include<iostream>
+
 using namespace std;
-int arr[1025][1025];
+int arr[1025][1025], dp[1025][1025];
+
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(0), cout.tie(0);
 	int n, m;
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			int a;
-			cin >> a;
-			arr[i][j] = arr[i][j - 1] + a;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			cin >> arr[i][j];
 		}
 	}
-	while (m--) {
-		int x1, x2, y1, y2;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		int x1, x2, y1, y2, ans;
 		cin >> x1 >> y1 >> x2 >> y2;
-		int sum = 0;
-		if (x1 == x2) {
-			cout<< arr[x2][y2] - arr[x2][y1 - 1]<<"\n";
-			continue;
-		}
-		for (int i = x1; i <= x2; i++) {
-			sum += arr[i][y2] - arr[i][y1 - 1];
-		}
-		cout << sum << "\n";
+		ans = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+		cout << ans << "\n";
 	}
 	return 0;
 }
