0135ae9330c953561eb31359fff79280b4013309
KimKiheon <cbj2741@naver.com>
2024-08-17 02:38:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 
 using namespace std;
-int arr[1025][1025], dp[1025][1025];
+int dp[1025][1025];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -10,12 +10,9 @@ int main() {
 	cin >> n >> m;
 	for (int i = 1; i < n + 1; i++) {
 		for (int j = 1; j < n + 1; j++) {
-			cin >> arr[i][j];
-		}
-	}
-	for (int i = 1; i < n + 1; i++) {
-		for (int j = 1; j < n + 1; j++) {
-			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+			int a;
+			cin >> a;
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + a;
 		}
 	}
 	for (int i = 0; i < m; i++) {
